404715a67279896b4a975afb33d652d56ab02d77
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-10-02 00:39:54 -0300

    feature: criação classes ListAdpter.kt e ListHolder.kt

---
+++ app/src/main/java/com/testemobile/githubjava/Adapter/ListAdpter.kt
@@ -0,0 +1,23 @@
+package com.testemobile.githubjava.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.testemobile.githubjava.Holder.ListHolder
+
+class ListAdpter:RecyclerView.Adapter<ListHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/testemobile/githubjava/Holder/ListHolder.kt
@@ -0,0 +1,10 @@
+package com.testemobile.githubjava.Holder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+
+
+}
