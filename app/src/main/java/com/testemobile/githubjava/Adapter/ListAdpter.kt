570406cdce503d0125e0f7942922887988f5a26d
Cleiton <clopesbraga@gmail.com>
2023-10-08 00:43:26 -0300

    feature(lista repositorio): subindo alteracao nas classes ListAdapter,ListFragment, ListHolder

--- app/src/main/java/com/testemobile/githubjava/Adapter/ListAdpter.kt
+++ app/src/main/java/com/testemobile/githubjava/Adapter/ListAdpter.kt
@@ -1,23 +1,41 @@
 package com.testemobile.githubjava.Adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.testemobile.githubjava.Holder.ListHolder
+import com.testemobile.githubjava.Model.RepositorioModel
+import com.testemobile.githubjava.databinding.UserListBinding
 
 class ListAdpter:RecyclerView.Adapter<ListHolder>(){
 
+    private var repoList: List<RepositorioModel> = listOf()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListHolder {
-        TODO("Not yet implemented")
+
+        val item = UserListBinding.inflate(LayoutInflater.from
+                                            (parent.context),parent,false)
+
+        return ListHolder(item)
+
     }
 
     override fun onBindViewHolder(holder: ListHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        holder.bind(repoList[position])
     }
 
-
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
+        return repoList.count()
+    }
+
+    fun atualizaListaRepositorio(list : List<RepositorioModel>){
+
+        repoList = list
+        notifyDataSetChanged()
+
     }
 
 }
---
+++ app/src/main/java/com/testemobile/githubjava/View/ListFragment.kt
@@ -0,0 +1,42 @@
+package com.testemobile.githubjava.View
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.testemobile.githubjava.Adapter.ListAdpter
+import com.testemobile.githubjava.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+    private lateinit var _binding: FragmentListBinding
+    private val binding get()= _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentListBinding.inflate(inflater,container,false)
+
+        //CHAMA RECYCLERVIEW DOS PRODUTOS
+        binding.ltvList.layoutManager = LinearLayoutManager(context)
+
+        //CHAMA ADAPTER DOS PRODUTOS
+        binding.ltvList.adapter= ListAdpter()
+
+        return binding.root
+
+    }
+}
+
+
+
