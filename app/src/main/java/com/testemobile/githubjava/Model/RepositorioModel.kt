effc52b3c679af5ed698461d2995b87e6365f198
Cleiton <clopesbraga@gmail.com>
2023-10-12 18:27:50 -0300

    inclusao das classes RetrofitService.kt e RepositorioModel.kt

--- app/src/main/java/com/testemobile/githubjava/Model/RepositorioModel.kt
+++ app/src/main/java/com/testemobile/githubjava/Model/RepositorioModel.kt
@@ -1,4 +1,19 @@
 package com.testemobile.githubjava.Model
 
+import com.google.gson.annotations.SerializedName
+
 class RepositorioModel {
+
+    @SerializedName(value="id")
+    var id :Int = 0
+
+    @SerializedName(value="username")
+    var username :String = ""
+
+    @SerializedName(value="description")
+    var description :String = ""
+
+    @SerializedName(value="topics")
+    var topics :String = ""
+
 }
