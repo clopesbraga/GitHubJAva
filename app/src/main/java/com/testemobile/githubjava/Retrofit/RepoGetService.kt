33d2e6ea837c8edff4ba352991cd897ba41c3b6f
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-10-21 20:09:40 -0300

    fix: ajuste na classe RepoGetService.kt

--- app/src/main/java/com/testemobile/githubjava/Retrofit/RepoGetService.kt
+++ app/src/main/java/com/testemobile/githubjava/Retrofit/RepoGetService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 
 interface RepoGetService {
 
-    @GET("search/repositories?q=language:Java&sort=stars&page=1/")
+    @GET("/search/repositories?q=language:Java&sort=stars&page=1/")
     fun list(): Call<List<RepositorioModel>>
 
 }
