fb05493f3fe233dbfd449f492303712abfd07b05
Cleiton <clopesbraga@gmail.com>
2023-10-22 02:12:30 -0300

    fix: atualizacao do Build_gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.testemobile.githubjava"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.testemobile.githubjava"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
