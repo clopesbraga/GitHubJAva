abd8729fb7b6305129b9ae3a477185a26878a6f1
Cleiton <clopesbraga@gmail.com>
2023-10-29 00:35:29 -0300

    feature: criado botao para testar chamada de requisicao.

--- app/src/main/java/com/testemobile/githubjava/View/MainActivity.kt
+++ app/src/main/java/com/testemobile/githubjava/View/MainActivity.kt
@@ -2,36 +2,37 @@ package com.testemobile.githubjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.testemobile.githubjava.Model.GitHubRepo
 import com.testemobile.githubjava.Retrofit.RepoGetService
 import com.testemobile.githubjava.Retrofit.RetrofitService
+import com.testemobile.githubjava.View.ListFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  btnlista: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val remote= RetrofitService.createService(RepoGetService::class.java)
-        val call: Call<GitHubRepo> = remote.getItems()
-
-        val response = call.enqueue(object : Callback<GitHubRepo>{
-
-            override fun onFailure(call:Call<GitHubRepo>, t:Throwable){
-                val s = t.message
-            }
-
-            override fun onResponse(
-                call: Call<GitHubRepo>,
-                response: Response<GitHubRepo>
-            ) {
-                val s = response.body()
-            }
-
-        })
-
+        btnlista = findViewById<Button>(R.id.btnlista)
 
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        btnlista.setOnClickListener{
+
+            val fragment = ListFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+    }
+
 }
