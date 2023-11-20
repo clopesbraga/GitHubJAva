3a80e516b72967d41c3db66cf4384b84044eef01
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-11-20 20:51:10 -0300

    feature(koin): ajustes nas classes ListFragment.kt e MainActivity.kt para o koin

--- app/src/main/java/com/testemobile/githubjava/View/ListFragment.kt
+++ app/src/main/java/com/testemobile/githubjava/View/ListFragment.kt
@@ -5,30 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.testemobile.githubjava.Adapter.ListAdpter
-import com.testemobile.githubjava.Model.GitHubRepo
-import com.testemobile.githubjava.Retrofit.RepoGetService
-import com.testemobile.githubjava.Retrofit.RetrofitService
 import com.testemobile.githubjava.ViewModel.RepositorioViewModel
 import com.testemobile.githubjava.databinding.FragmentListBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import org.koin.android.ext.android.inject
+
 
 class ListFragment : Fragment() {
 
     private lateinit var _binding: FragmentListBinding
     private val binding get()= _binding
     private val adpater = ListAdpter()
-    private lateinit var viewModel : RepositorioViewModel
+    private val viewModel : RepositorioViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel =  ViewModelProvider(this).get(RepositorioViewModel::class.java)
-        viewModel.requestGitHubRepo()
+       viewModel.requestGitHubRepo()
 
     }
 
--- app/src/main/java/com/testemobile/githubjava/View/MainActivity.kt
+++ app/src/main/java/com/testemobile/githubjava/View/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.testemobile.githubjava
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.testemobile.githubjava.Model.GitHubRepo
-import com.testemobile.githubjava.Retrofit.RepoGetService
-import com.testemobile.githubjava.Retrofit.RetrofitService
+import androidx.appcompat.app.AppCompatActivity
 import com.testemobile.githubjava.View.ListFragment
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
 
