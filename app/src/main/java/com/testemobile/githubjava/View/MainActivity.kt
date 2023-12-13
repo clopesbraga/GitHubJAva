9563faf364b3cf926f575e50d4b16178711c53d4
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-12-13 00:19:21 -0300

    feature(activity_main): inclusao da correçao em activity_main.xml e MainActivity.kt

--- app/src/main/java/com/testemobile/githubjava/View/MainActivity.kt
+++ app/src/main/java/com/testemobile/githubjava/View/MainActivity.kt
@@ -2,6 +2,7 @@ package com.testemobile.githubjava
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.testemobile.githubjava.View.ListFragment
@@ -10,13 +11,20 @@ import com.testemobile.githubjava.ViewModel.RepositorioViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var  btnlista: Button
+    lateinit var  btnAnterior: Button
+    lateinit var  btnProximo: Button
+    lateinit var txtNumPage : TextView
+    lateinit var txtNum : TextView
     private lateinit var viewModel: RepositorioViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnlista = findViewById<Button>(R.id.btnlista)
+        btnAnterior = findViewById(R.id.btnAnterior)
+        btnProximo =  findViewById(R.id.btnProximo)
+
+        txtNumPage = findViewById((R.id.txtNumPage))
+        txtNum =  findViewById((R.id.txtPage))
 
     }
 
@@ -24,17 +32,27 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         viewModel = ViewModelProvider(this).get(RepositorioViewModel::class.java)
-        viewModel.requestGitHubRepo()
 
-        btnlista.setOnClickListener{
+        btnProximo.setOnClickListener {
+            txtNum.text="2"
+            buttonAction("2")
+        }
 
+        btnAnterior.setOnClickListener {
+            txtNum.text="1"
+            buttonAction("1")
+        }
+
+    }
+
+    fun buttonAction(page: String){
+
+            viewModel.requestGitHubRepo(page)
             val fragment = ListFragment()
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .addToBackStack(null)
                 .commit()
-        }
-
     }
 
 }
