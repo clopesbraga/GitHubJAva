bb3c09f070a499852380428c93856b74e45aa2b3
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-12-22 15:45:38 -0300

    atilização do build.gradle para 8.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
     //DEPENDENCIAS DE TESTE
     testImplementation("junit:junit:4.13.2")
+    implementation("org.mockito:mockito-core:5.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
